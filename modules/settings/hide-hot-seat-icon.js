c5029fb3ef90519e2940b09ee128962e386f0242
mrjaksauce <68178310+mrjaksauce@users.noreply.github.com>
2021-08-03 11:31:43 +1200

    Update hide-hot-seat-icon.js

--- modules/settings/hide-hot-seat-icon.js
+++ modules/settings/hide-hot-seat-icon.js
@@ -1,30 +1,25 @@
 import { Settings } from "./settings.js";
 
 export class HideIconMode{
-
-    static _OnSettingsChanged (enabled)  {
+    static _OnSettingsChanged (enabled)
+    {
         let hotSeatPlayerName = Settings.RetreiveHotSeatPlayerName();
         let hotSeatPlayerUser = game.users.players.find(t => t.name === hotSeatPlayerName);
-
         let pcs = game.actors.entities.filter(t => t.isPC && t.data.permission[hotSeatPlayerUser.id] == "3");
-
         let isUserGm = game.user.isGM;
 
         if(!isUserGm){   
-            pcs.forEach(element => {
+            pcs.forEach(element => 
+            {
                 let tokens = canvas.tokens.placeables
                 let pcToken = tokens.find(t => t.actor && t.actor._id == element._id);
                 this.UpdateTokenVisibility(pcToken);
             });
         }
-
     }
-
-    static UpdateTokenVisibility(token) {
+    static UpdateTokenVisibility(token) 
+    {
         let gameSettingsEnabled = Settings.HidePlayer();
         token.icon.visible = !gameSettingsEnabled;
     }
-
-
-
 }
